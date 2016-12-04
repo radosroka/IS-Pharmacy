a57d4ce6c3b4725ce53b9eac7a55a28fea7b14ca
tomassykora <tms.sykora@gmail.com>
2016-12-04 11:58:33 +0100

    repaired searching in different browsers

--- www/js/searchBox.js
+++ www/js/searchBox.js
@@ -1,7 +1,11 @@
 function searchItems() {
-	var text = document.getElementById('searchBoxText').value;
-	var location = "lekaren/find-items/?searchedText=";
-	window.location.pathname = location + text;
+
+
+	  var text = document.getElementById('searchBoxText').value;
+	  var protocol = window.location.protocol;
+	  var hostname = window.location.hostname;
+	  var url = protocol + '//' + hostname + "/lekaren/find-items?searchedText=" + text;
+	  window.location.href = url;
 }
 
 document.getElementById("searchBoxText")
